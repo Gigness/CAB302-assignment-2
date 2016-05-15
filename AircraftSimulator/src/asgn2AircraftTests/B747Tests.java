35232a700a2e51518d334bd433ebb08da61aed73
Paul Foo <dev.paul.foo@gmail.com>
2016-05-15 16:21:42 +1000

    setup testing packages

---
+++ AircraftSimulator/src/asgn2AircraftTests/B747Tests.java
@@ -0,0 +1,111 @@
+package asgn2AircraftTests;
+
+import asgn2Aircraft.Aircraft;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Gigness on 15/05/2016.
+ */
+public class B747Tests extends Aircraft {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void cancelBooking() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void confirmBooking() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void finalState() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void flightEmpty() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void flightFull() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void flyPassengers() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getBookings() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getNumBusiness() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getNumEonomy() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getNumFirst() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getNumPassengers() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getNumPremium() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getPassengers() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getStatus() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void hasPassenger() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void initialState() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void seatsAvailable() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void toString() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void upgradeBookings() throws Exception {
+
+    }
+}
---
+++ AircraftSimulator/src/asgn2PassengersTests/EconomyTests.java
@@ -0,0 +1,138 @@
+package asgn2PassengersTests;
+
+import asgn2Passengers.Passenger;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Gigness on 15/05/2016.
+ */
+public class EconomyTests extends Passenger {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void noSeatsMsg() throws Exception {
+
+    }
+
+    @Test
+    public void upgrade() throws Exception {
+
+    }
+
+    @Test
+    public void cancelSeat() throws Exception {
+
+    }
+
+    @Test
+    public void confirmSeat() throws Exception {
+
+    }
+
+    @Test
+    public void flyPassenger() throws Exception {
+
+    }
+
+    @Test
+    public void getBookingTime() throws Exception {
+
+    }
+
+    @Test
+    public void getConfirmationTime() throws Exception {
+
+    }
+
+    @Test
+    public void getDepartureTime() throws Exception {
+
+    }
+
+    @Test
+    public void getEnterQueueTime() throws Exception {
+
+    }
+
+    @Test
+    public void getExitQueueTime() throws Exception {
+
+    }
+
+    @Test
+    public void getPassID() throws Exception {
+
+    }
+
+    @Test
+    public void isConfirmed() throws Exception {
+
+    }
+
+    @Test
+    public void isFlown() throws Exception {
+
+    }
+
+    @Test
+    public void isNew() throws Exception {
+
+    }
+
+    @Test
+    public void isQueued() throws Exception {
+
+    }
+
+    @Test
+    public void isRefused() throws Exception {
+
+    }
+
+    @Test
+    public void noSeatsMsg1() throws Exception {
+
+    }
+
+    @Test
+    public void queuePassenger() throws Exception {
+
+    }
+
+    @Test
+    public void refusePassenger() throws Exception {
+
+    }
+
+    @Test
+    public void toString() throws Exception {
+
+    }
+
+    @Test
+    public void upgrade1() throws Exception {
+
+    }
+
+    @Test
+    public void wasConfirmed() throws Exception {
+
+    }
+
+    @Test
+    public void wasQueued() throws Exception {
+
+    }
+
+    @Test
+    public void copyPassengerState() throws Exception {
+
+    }
+}
